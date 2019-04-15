c024688ce6062b77e739c15ebf7870118a88658f
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-15 05:06:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ end
 def assing_rooms(speakers)
   
   speakers.collect.each_with_index do { | speaker, index |
-  "Hello, #{speaker}! You'll be assigned to room #(index + 1)!")}
+  "Hello, #{speaker}! You'll be assigned to room #{index + 1}!")}
 
 
 
