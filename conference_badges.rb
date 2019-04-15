12b8962e37ce4990127c1d987991e16cc517b30c
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-15 04:51:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creater(speakers)
+  new_array = []
+  speakers.each do |speaker|
+    new_array.push(badge_maker)
+  end
+  return new_array
+end
+  
+  
+  #for speakers.length do
+  #return "Hello, my name is #{speaker[i]}."
+#end
+
+
+
