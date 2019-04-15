0dc2c4899a079102d66ef44917469ba741bd1d72
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-15 05:05:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,28 +7,12 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(speakers)
-  speakers.collect 
-  speakers.each do |speaker|
-    new_array.push(badge_maker(speaker))
+  speakers.collect do |speaker|
+    badge_maker(speaker)
   end
-  return new_array
+
 end
   
-def assign_rooms(speakers)
-  new_array = []
-  room_count = 1
-  speakers.each do |speaker|
-    new_array.push("Hello, #{speaker}! You'll be assigned to room #{room_count}!")
-    room_count += 1
-    end
-  return new_array
-end
-
-def assing_rooms(speakers)
-  new_array
-  speakers.each_with_index do { |speaker, index| }
-  new_array.push("Hello, #{speaker}! You'll be assigned to room (index + 1)!"))
-
 
 
 
