9116375c6c7572d8da68c104dc0ab7aa3a4811f8
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-15 05:07:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ def batch_badge_creator(speakers)
 end
   
 
-def assing_rooms(speakers)
+def assi_rooms(speakers)
   speakers.collect.each_with_index do { | speaker, index |
   "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"}
 end
